e7792d4e9c4497c06df3632b289c963611095fb0
Dmitrii <doronin_dima@mail.ru>
2022-04-19 22:42:09 +0300

    Property

--- components/Property.jsx
+++ components/Property.jsx
@@ -23,23 +23,12 @@ const Property = ({
   },
 }) => (
   <Link href={`/property/${externalID}`} passHref>
-    <Flex
-      flexWrap="wrap"
-      w="420px"
-      p="5"
-      paddingTop="0"
-      justifyContent="flex-start"
-      cursor="pointer"
-    >
+    <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
       <Box>
-        <Image
-          src={coverPhoto ? coverPhoto.url : DefaultImage}
-          width={400}
-          height={400}
-          alt="house"
-        />
+        <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt="house" />
       </Box>
-      <Box w="full">
+
+      <Box w="full">  
         <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
           <Flex alignItems="center">
             <Box paddingRight="3" color="green.400">
@@ -50,11 +39,17 @@ const Property = ({
             </Text>
           </Flex>
           <Box>
-            <Avatar size="sm" src={agency?.logo?.url} />
+            <Avatar w='250px' src={agency?.logo?.url} />
           </Box>
         </Flex>
-        
+        <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400" >
+          {rooms} <FaBed/> | {baths} <FaBath/> | {millify(area)} sqft <BsGridFill/> 
+        </Flex>
+        <Text fontSize="lg">
+          {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+        </Text>
       </Box>
+
     </Flex>
   </Link>
 );
